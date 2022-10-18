4ac9e1b21535839536c109729015bca74419d1aa
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-10-18 11:36:23 +0300

    commit tree 13da4477303b7ba427d148f3bd7a4a46ba5ed189

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+require "pry"
+def badge_maker name
+    formatted = "Hello, my name is #{name}."
+    puts formatted
+    formatted
+end
+
+def batch_badge_creator(arr)
+    arr.map {|a| "Hello, my name is #{a}."}
+end
+
+def assign_rooms arr
+    arr.map.with_index {|a, i| "Hello, #{a}! You'll be assigned to room #{i+1}!"}
+end
+
+def printer arr
+    arr.each {|a| puts "Hello, my name is #{a}."}
+    arr.map.with_index {|a, i| puts "Hello, #{a}! You'll be assigned to room #{i+1}!"}
+end
+
+# binding.pry
